33ba9f08bfa0d9a5844f66b531b4fb432f2b789e
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-07 08:52:29 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+const num3 = 36
+const num4 = 8
+const multiply = num2*num1
+const random = Math.floor(Math.random()*10) + 1
+const mod = num3%num4
+const max = Math.max(20, 1, 3)
